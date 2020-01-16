07ce072045e1fc48007532eaf3895adb758c1358
Fabiana Ferreira <fabiana.ferreira@poli.ufrj.br>
2020-01-16 09:39:24 -0200

    processing annotations and adding logging to scripts

--- Classifier/extract_features.py
+++ Classifier/extract_features.py
@@ -3,6 +3,12 @@ from glob import glob
 import pandas as pd
 import re
 from tqdm import tqdm
+import logging
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s',
+                    filename="/home/fabiana/Desktop/projeto-final-src/Classifier/InceptionV3Features.log",
+                    filemode='a')
 
 
 def getFrameNumber(path):
@@ -22,7 +28,11 @@ def buildSequence(frameList):
         sequence.append(features)
     return np.array(sequence)
 
+
 proc_csv = pd.read_pickle('dataset_with_file_list.pkl')
+
+logging.info("Starting routine for features extraction with InceptionV3")
+
 model = Extractor()
 
 proc_csv.set_index('palavra', inplace=True)
@@ -31,5 +41,9 @@ folder = '/home/fabiana/Desktop/projeto-final-src/Classifier/InceptionV3_Feature
 for n in ['5', '10', '15']:
     print('Number of keyframes: ' + n)
     for palavra, frameList in tqdm(proc_csv[f'files_list_{n}'].iteritems(), total=len(proc_csv)):
+        if (len(frameList) < int(n)):
+            print(f"Word {palavra} got less than {n} key frames")
+            logging.warning(f"Word {palavra} got less than {n} key frames")
+
         seq = buildSequence(frameList)
         np.save(f'{folder}/{n}/{palavra}', seq)
