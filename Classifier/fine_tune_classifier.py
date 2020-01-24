58efdef1ab30945c7c689c75f754ed174290d5ca
Fabiana Ferreira <fabiana.ferreira@poli.ufrj.br>
2020-01-24 02:59:29 -0300

    testing reduceLROnPlateau callback

--- Classifier/fine_tune_classifier.py
+++ Classifier/fine_tune_classifier.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, LambdaCallback
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, LambdaCallback, ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from datetime import datetime
 
@@ -30,7 +30,7 @@ def createModel(img_rows=224, img_cols=224, channel=3, num_classes=None):
     model.add(Dense(num_classes, activation='softmax'))
 
     # Learning rate is changed to 0.001
-    sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = SGD(lr=0.001, decay=0, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
@@ -58,6 +58,7 @@ train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.3,
     zoom_range=0.3,
+    # samplewise_center=True,
     horizontal_flip=False,
     vertical_flip=False,
     validation_split=0.2
@@ -97,24 +98,27 @@ print_weights = LambdaCallback(on_epoch_end=lambda batch, logs: print(model.laye
 early_stopping = EarlyStopping(monitor='val_loss', patience=epochs_to_wait_for_improvement)
 checkpoint = ModelCheckpoint(f'{models_path}/{model_name}.h5', monitor='val_loss', save_best_only=True, mode='min')
 csv_logger = CSVLogger(f'{logging_path}/{model_name}.log')
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4,
+                              patience=5, min_lr=1e-5)
 
-callbacks = [early_stopping, checkpoint, csv_logger]
+# callbacks = [early_stopping, checkpoint, csv_logger]
+callbacks = [reduce_lr, checkpoint, csv_logger]
 
 print('Training model... You should get a coffee...')
 # Fit the model
 print(model.summary())
 # print(model_name)
-#exit(1)
+# exit(1)
 model.fit_generator(
     generator=train_gen,
     steps_per_epoch=train_gen.samples // BATCH_SIZE,
-    epochs=1000,
+    epochs=300,
     verbose=1,
     validation_data=validation_gen,
     validation_steps=validation_gen.samples // BATCH_SIZE,
     callbacks=callbacks,
     # class_weight=[12.39411284, 5.43687231, 11.48333333, 4.59194184, 9.109375, 5.06617647, 8.10588235]
-    #class_weight=[2, 1, 2, 1, 1.5, 1, 1.5]
+    # class_weight=[2, 1, 2, 1, 1.5, 1, 1.5]
     class_weight=[1.76699708, 0.7763886, 1.63858549, 0.65533498, 1.30395869,
                   0.72539257, 1.15690616]
 )
