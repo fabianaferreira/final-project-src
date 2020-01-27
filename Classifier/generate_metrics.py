f82f880060541998b36f5895c3cb7440b3eaded3
Fabiana Ferreira <fabiana.ferreira@poli.ufrj.br>
2020-01-27 11:23:21 -0300

    separating script for confusion matrix and metrics

--- Classifier/generate_metrics.py
+++ Classifier/generate_metrics.py
@@ -1,5 +1,5 @@
 from DataGenerator import DataGenerator
-from ConfusionMatrix import ConfusionMatrix
+from sklearn.metrics import classification_report
 from tensorflow.keras.models import load_model
 import numpy as np
 import pandas as pd
@@ -7,10 +7,17 @@ import pandas as pd
 BATCH_SIZE = 64
 SUBSET = True
 MODELS_DIR_PATH = './models/'
-CONFUSION_MATRIX_DIR = './Confusion_Matrix/'
+METRICS_DIR = './Metrics/'
+LOGS_DIR = './logs/'
 
 
-def generate_plot(model_name):
+def top_n_accuracy(y_true, y_pred, N=1):
+    top_n_ix = np.argpartition(y_pred, -N)[:, -N:]
+    is_y_in_topn = [y in top_n_ix[i, :] for i, y in enumerate(y_true)]
+    return np.sum(is_y_in_topn) / len(y_true)
+
+
+def generate_metrics(model_name):
     # Loading labels and data
     if SUBSET:
         labels = np.load('./Plots/labels_CM_subset.npy', allow_pickle=True)
@@ -35,22 +42,23 @@ def generate_plot(model_name):
     print('Predicting...')
     result = model.predict_generator(test_gen)
 
-    fig_name = 'confusion_matrix_model_' + model_name.rsplit('/', 1)[1][:-3] + ('_subset' if SUBSET else '')
-    # report_name = 'classification_report_' + model_name.rsplit('/', 1)[1][:-3] + ('_subset' if SUBSET else '')
-    cm = ConfusionMatrix(labels, y_pred=y_test, y_true=result)
+    aux_model_name = model_name.rsplit('/', 1)[1][:-3]
 
-    print('Saving matrix...')
-    cm.save_matrix(filename=fig_name)
+    report_name = 'classification_report_' + aux_model_name + ('_subset' if SUBSET else '')
+    history_name = 'history_' + aux_model_name + ('_subset' if SUBSET else '')
 
-    # print('Calculating other metrics...')
-    # report = classification_report(target_names=labels, y_true=np.argmax(result, axis=1),
-    #                                y_pred=np.argmax(y_test, axis=1), output_dict=True)
-    # # print(report)
-    #
-    # print('Saving report...')
-    # df = pd.DataFrame(report).transpose()
-    # df.to_csv(CONFUSION_MATRIX_DIR + report_name + '.csv')
+    print('Calculating other metrics...')
+    report = classification_report(target_names=labels, y_true=np.argmax(result, axis=1),
+                                   y_pred=np.argmax(y_test, axis=1), output_dict=True)
+
+    print('Saving report...')
+    df = pd.DataFrame(report).transpose()
+    df.to_csv(METRICS_DIR + report_name + '.csv')
+
+    print('Saving history...')
+    df = pd.read_csv(LOGS_DIR + aux_model_name + '.log')
+    df.to_csv(METRICS_DIR + history_name + '.csv')
 
 
 model = MODELS_DIR_PATH + 'fine_tune_VGG16_no_edge_frames_2020-01-23-19:27:25.h5'
-generate_plot(model)
+generate_metrics(model)
